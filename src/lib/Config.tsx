b8cfbbf9abc21971e3c81d636eb852bff5dfd86b
Fä Bu <87422685+fabu21@users.noreply.github.com>
2022-06-05 18:49:08 -0500

    extracted API Url to config

--- src/lib/Config.tsx
+++ src/lib/Config.tsx
@@ -1,2 +1,3 @@
 export const API_URL: string = 'api.mainnet.galoy.io/graphql'
 export const AVAILABLE_CURRENCIES: {label: string, value:string}[] = [{value: 'GTQ', label: 'QTG'},{value:'PEN', label:'PEN'}]
+export const API_YADIO_URL: string = 'https://api.yadio.io/'
